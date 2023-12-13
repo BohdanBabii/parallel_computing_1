a43666eb3a16b1cb673055df941eb65b5c4b9cc9
Shad00Z <lucasobitz@gmx.de>
2023-12-13 07:42:46 +0100

    Finished Stencil Task with Script

---
+++ src/week_8/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=myjob
+#SBATCH --output=myjob.out
+#SBATCH --error=myjob.err
+#SBATCH --partition=s_hadoop
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=72
+
+# Load any necessary modules (if needed)
+module load mpi/openmpi/4.1.2-gcc-10.2.0
+mpicxx -o pi.o pi.cpp
+
+# Enter your executable commands here
+# Execute the compiled program
+mpirun -np 10 ./pi.o
